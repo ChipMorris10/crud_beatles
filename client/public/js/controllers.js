d9569a92c80f12c0e2692b3769697460122b55b4
ChipMorris10 <goldcomac@mac.com>
2015-10-01 17:13:37 -0600

    get all is functional

--- client/public/js/controllers.js
+++ client/public/js/controllers.js
@@ -11,6 +11,7 @@ app.controller('songController', function($scope, $timeout, HTTPFactory) {
     .then(function(response) {
       $scope.songs = response.data;
       console.log(response.data);
+      console.log("Console log from Line 14");
     });
   };
 
@@ -19,6 +20,22 @@ app.controller('songController', function($scope, $timeout, HTTPFactory) {
  function messageTimeout() {
   $scope.success = false;
  }
+
+ $scope.postSong = function() {
+  var payload = $scope.song;
+  HTTPFactory.post('/api/songs', payload)
+  .then(function(response) {
+    $scope.songs.push(response.data);
+    $scope.song = {};
+    $scope.success = true;
+    $scope.message = "I added a new song!";
+    timeout(messageTimeout, 5000);
+    getSongs('api/songs');
+    console.log(response.data);
+  });
+ };
+
+
 
 
 
